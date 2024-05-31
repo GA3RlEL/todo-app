e4a23dd47ed4b169d22a4a552cb7ffc586177cd3
GA3RlEL <kruszkagabrys@gmail.com>
2024-05-31 18:01:02 +0200

    added calendar component

---
+++ frontend/src/app/components/calendar/calendar.component.ts
@@ -0,0 +1,96 @@
+import { DatePipe } from '@angular/common';
+import { Component } from '@angular/core';
+
+
+interface day {
+  dayNumber: number;
+  isPrevious: boolean;
+  month: number;
+  year: number;
+}
+
+@Component({
+  selector: 'app-calendar',
+  standalone: true,
+  imports: [DatePipe],
+  templateUrl: './calendar.component.html',
+  styleUrl: './calendar.component.css'
+})
+export class CalendarComponent {
+  daysName = ['Mon', 'Tue', 'Wed', "Thu", "Fri", "Sat", "Sun"];
+  daysToPrint: day[] = [];
+
+  isVisible = false;
+
+  year = new Date().getFullYear();
+  month = new Date().getMonth();
+
+  selectedMonth = new Date().getMonth();
+  selectedDate = new Date(this.year, this.selectedMonth + 1, 0)
+  selectedYear = new Date().getFullYear();
+
+  firstDayOfMonth = new Date(this.year, this.selectedMonth, 1)
+
+  initialDayOfMonth = new Date(new Date().getFullYear(), new Date().getMonth(), 1)
+
+  onNextMonth() {
+    this.selectedMonth += 1;
+    this.selectedDate = new Date(this.year, this.selectedMonth + 1, 0);
+    this.firstDayOfMonth = new Date(this.year, this.selectedMonth, 1)
+    this.daysToPrint = this.calculateDays(this.firstDayOfMonth)
+    this.selectedYear = new Date(this.year, this.selectedMonth + 1, 0).getFullYear();
+
+  }
+
+  onPreviousMonth() {
+    this.selectedMonth -= 1;
+    this.selectedDate = new Date(this.year, this.selectedMonth + 1, 0);
+    this.firstDayOfMonth = new Date(this.year, this.selectedMonth, 1)
+    this.daysToPrint = this.calculateDays(this.firstDayOfMonth)
+    this.selectedYear = new Date(this.year, this.selectedMonth + 1, 0).getFullYear();
+  }
+
+  onShowCalendar() {
+    this.isVisible = !this.isVisible;
+    this.year = new Date().getFullYear();
+    this.selectedYear = new Date().getFullYear();
+    this.selectedMonth = new Date().getMonth();
+    this.selectedDate = new Date(this.year, this.selectedMonth + 1, 0)
+    this.daysToPrint = this.calculateDays(this.initialDayOfMonth);
+  }
+
+  calculateDays(firstDayOfMonth: Date) {
+    const dayName = firstDayOfMonth.toDateString().slice(0, 3);
+    const previousMonthDays = new Date(this.year, this.selectedMonth, 0).getDate();
+    const actuallMonthDays = new Date(this.year, this.selectedMonth + 1, 0).getDate();
+
+    const difference = this.daysName.findIndex((day) => day === dayName)
+
+    const days: day[] = []
+    let counterPrevious = difference;
+    let counterActuall = 1;
+
+    for (let i = 0; i < actuallMonthDays + difference; i++) {
+      if (counterPrevious > 0) {
+        days.push({
+          dayNumber: previousMonthDays - counterPrevious + 1,
+          isPrevious: true,
+          month: this.selectedMonth, year: this.year
+        })
+        counterPrevious--;
+
+      } else {
+        days.push({ dayNumber: counterActuall, isPrevious: false, month: this.selectedMonth, year: this.year })
+        counterActuall++;
+      }
+    }
+    return days;
+  }
+
+  onSelectDate(day: number, month: number, year: number) {
+    this.isVisible = false;
+
+    console.log(new Date(year, month, day));
+  }
+
+}
--- frontend/src/app/todos-section/todos-section.component.ts
+++ frontend/src/app/todos-section/todos-section.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { CalendarComponent } from "../components/calendar/calendar.component";
 
 @Component({
   selector: 'app-todos-section',
   standalone: true,
-  imports: [],
   templateUrl: './todos-section.component.html',
-  styleUrl: './todos-section.component.css'
+  styleUrl: './todos-section.component.css',
+  imports: [CalendarComponent]
 })
 export class TodosSectionComponent {
 
