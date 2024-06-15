7f3cf3fd1b6932310c0ba52ac69d69aa4bd573c5
GA3RlEL <kruszkagabrys@gmail.com>
2024-06-15 15:10:56 +0200

    .env fix

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,8 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
+(window as any).process = { env: { DEBUG: undefined } };
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
-
-
+bootstrapApplication(AppComponent, appConfig).catch((err) =>
+  console.error(err)
+);
